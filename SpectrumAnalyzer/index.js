68e38d600dad0cce713dff05cbd23b25896db176
clonq <ovi.comes@gmail.com>
2018-07-11 23:46:50 -0400

    blob handling

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,5 +1,12 @@
-module.exports = function (context, req) {
-    context.log('analyzing audio spectrum...');
+const storage = require('azure-storage');
+const blobService = storage.createBlobService();
+
+module.exports = (context, myBlob) => {
+    const blobName = context.bindingData.name;
+    const contentType = context.bindingData.properties.contentType;
+
+    context.log(`analyzing audio spectrum for ${contentType}: ${blobName}`);
+
     context.res = {
         status: 200,
         body: "spectrum analyzer output"
