b1d77e620b6229433beac3ab59a04c326554ea08
clonq <ovi.comes@gmail.com>
2018-07-14 00:08:21 -0400

    wip

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,11 +1,13 @@
-const storage = require('azure-storage');
-const blobService = storage.createBlobService();
+// const storage = require('azure-storage');
+// const blobService = storage.createBlobService();
 
 module.exports = (context, myBlob) => {
-    const blobName = context.bindingData.name;
-    const contentType = context.bindingData.properties.contentType;
+    // const blobName = context.bindingData.name;
+    // const contentType = context.bindingData.properties.contentType;
 
-    context.log(`analyzing audio spectrum for ${contentType}: ${blobName}`);
+    // context.log(`analyzing audio spectrum for ${contentType}: ${blobName}`);
+
+    context.log(`analyzing audio spectrum`);
 
     context.res = {
         status: 200,
