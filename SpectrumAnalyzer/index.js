f99c8c7c759525050809af361fce16c0f5b65856
clonq <ovi.comes@gmail.com>
2018-07-14 00:19:09 -0400

    http bindings

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,12 +1,9 @@
 // const storage = require('azure-storage');
 // const blobService = storage.createBlobService();
 
-// module.exports = (context, myBlob) => {
-module.exports = (context) => {
+module.exports = function (context, req) {
     // const blobName = context.bindingData.name;
     // const contentType = context.bindingData.properties.contentType;
-
-    // context.log(`analyzing audio spectrum for ${contentType}: ${blobName}`);
 
     context.log(`analyzing audio spectrum`);
 
