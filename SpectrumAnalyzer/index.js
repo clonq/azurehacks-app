a8c988c40410c62545c3d3d9f4f0ba1071a09035
clonq <ovi.comes@gmail.com>
2018-07-16 21:49:32 -0400

    wip

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,19 +1,6 @@
 // const storage = require('azure-storage');
 // const blobService = storage.createBlobService();
 
-// module.exports = function (context, req) {
-//     // const blobName = context.bindingData.name;
-//     // const contentType = context.bindingData.properties.contentType;
-
-//     context.log(`analyzing audio spectrum`);
-
-//     context.res = {
-//         status: 200,
-//         body: "spectrum analyzer output"
-//     };
-//     context.done();
-// };
-
 const SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
 const StorageBlobCreatedEvent = "Microsoft.Storage.BlobCreated";
 const CustomTopicEvent = "Contoso.Items.ItemReceivedEvent";
