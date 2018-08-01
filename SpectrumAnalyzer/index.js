c2e67376aaae807352d95a87c0551caa274aeae5
clonq <ovi.comes@gmail.com>
2018-08-01 14:03:33 -0400

    wip

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -4,12 +4,11 @@ const AudioContext = require('web-audio-api').AudioContext;
 module.exports = function(context) {
     context.log(`analyzing audio spectrum`);
     let audioContext = new AudioContext();
-    context.log(String(context.bindings.mp4).substring(0, 100))
-    // audioContext.decodeAudioData(context.bindings.mp4, audioBuffer => {
-    //     context.log(`channels: ${audioBuffer.numberOfChannels}`);
-    //     context.log(`samplerate: ${audioBuffer.sampleRate}`);
-    //     context.log(`length: ${audioBuffer.length}`);
-    //     context.log(`duration: ${audioBuffer.duration}`);
+    audioContext.decodeAudioData(context.bindings.mp4, audioBuffer => {
+        context.log(`channels: ${audioBuffer.numberOfChannels}`);
+        context.log(`samplerate: ${audioBuffer.sampleRate}`);
+        context.log(`length: ${audioBuffer.length}`);
+        context.log(`duration: ${audioBuffer.duration}`);
         context.done();
-    // });
+    });
 }
