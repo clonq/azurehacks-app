26106f9710e427cdf7afcec5860904ce318bfd08
clonq <ovi.comes@gmail.com>
2018-08-01 12:04:35 -0400

    testing external modules installation

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,45 +1,7 @@
-// const storage = require('azure-storage');
-// const blobService = storage.createBlobService();
+const _ = require('lodash');
 
-const SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
-const StorageBlobCreatedEvent = "Microsoft.Storage.BlobCreated";
-const CustomTopicEvent = "Contoso.Items.ItemReceivedEvent";
-
-module.exports = function (context, req) {
-
-    var parsedReq = JSON.parse(req['rawBody']);
-    context.log('request:' + parsedReq);
-
-    parsedReq.forEach(eventGridEvent => {
-        var eventData = eventGridEvent.data; 
-        // Deserialize the event data into the appropriate type based on event type using if/elif/else
-        if (eventGridEvent.eventType == SubscriptionValidationEvent) {
-            context.log('Got SubscriptionValidation event data, validationCode: ' + eventData.validationCode + ', topic: ' + eventGridEvent.topic); 
-            context.res = {
-                validationResponse: eventData.validationCode
-            };
-        } else if (eventGridEvent.eventType == StorageBlobCreatedEvent) {
-            context.log('Got Blobcreated event data, blob URI ' + eventData.url);
-        } else if (eventGridEvent.eventType == CustomTopicEvent) {
-            context.log('Got ContosoItemReceived event data, item SKU ' + eventData.itemSku);
-        }
-    });
-
+module.exports = function(context, wav) {
+    context.log(`analyzing audio spectrum`);
+    context.log(_);
     context.done();
-    
-
-    // const blobName = context.bindingData.name;
-    // const contentType = context.bindingData.properties.contentType;
-
-    // console.log(`analyzing audio spectrum, blobName: ${blobName}, contentType: ${contentType}`);
-    // // context.log(`analyzing audio spectrum, blobName: ${blobName}, contentType: ${contentType}`);
-
-    // context.res = {
-    //     status: 200,
-    //     body: "spectrum analyzer output"
-    // };
-};
-
-
-
-
+}
