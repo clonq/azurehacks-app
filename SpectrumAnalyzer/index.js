328dc4a1df1a199df56740d70f07de9824acf47d
clonq <ovi.comes@gmail.com>
2018-08-01 13:48:48 -0400

    wip

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,15 +1,8 @@
 const _ = require('lodash');
 const AudioContext = require('web-audio-api').AudioContext;
 
-let audioContext = new AudioContext();
-
-module.exports = function(context, wav) {
+module.exports = function(context) {
     context.log(`analyzing audio spectrum`);
-    audioContext.decodeAudioData(wav, audioBuffer => {
-        context.log(`channels: ${audioBuffer.numberOfChannels}`);
-        context.log(`samplerate: ${audioBuffer.sampleRate}`);
-        context.log(`length: ${audioBuffer.length}`);
-        context.log(`duration: ${audioBuffer.duration}`);
-        context.done();
-    });
+    let audioContext = new AudioContext();
+    context.log(context.bindings.wav);
 }
