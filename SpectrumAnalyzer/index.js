38331c3ec3f31f4261ff3ed5dd4d91e6dea523e5
clonq <ovi.comes@gmail.com>
2018-08-01 13:32:37 -0400

    testing audio context

--- SpectrumAnalyzer/index.js
+++ SpectrumAnalyzer/index.js
@@ -1,7 +1,14 @@
 const _ = require('lodash');
+const AudioContext = require('web-audio-api').AudioContext;
+
+let audioContext = new AudioContext();
 
 module.exports = function(context, wav) {
     context.log(`analyzing audio spectrum`);
-    context.log(_);
+    audioContext.decodeAudioData(wav, audioBuffer => {
+    console.log(`channels: ${audioBuffer.numberOfChannels}`);
+    console.log(`samplerate: ${audioBuffer.sampleRate}`);
+    console.log(`length: ${audioBuffer.length}`);
+    console.log(`duration: ${audioBuffer.duration}`);
     context.done();
 }
